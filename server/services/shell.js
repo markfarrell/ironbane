b1ee918854152ed69f4b17541a1a1b87b61d1eee
Mark Farrell <m4farrel@uwaterloo.ca>
2019-01-07 12:16:14 -0500

    Update shell service

--- server/services/shell.js
+++ server/services/shell.js
@@ -1,22 +1,28 @@
 angular
     .module('server.services.shell', [
+        'underscore',
+        'three',
+        'engine.entity-builder',
     	'services.npcs',
     	'services.items',
-    	'server.services.zones'
-    ]).run(['$log', 'NPCService', 'ItemService', 'ZonesService', function($log, NPCService, ItemService, ZonesService) {
+    	'server.services.zones',
+        'server.services.activeWorlds'
+    ]).run(['_', 'THREE', 'EntityBuilder', '$log', 'NPCService', 'ItemService', 'ZonesService', '$activeWorlds', function(_, THREE, EntityBuilder, $log, NPCService, ItemService, ZonesService, $activeWorlds) {
     	'use strict';
 
     	// Services available to shell
     	var services = {
+            'engine.entity-builder' : EntityBuilder,
     		'services.npcs' : NPCService,
     		'services.items' : ItemService,
-    		'server.services.zones' : ZonesService
+    		'server.services.zones' : ZonesService,
+            'server.services.activeWorlds' : $activeWorlds
     	};
 
     	Meteor.methods({
     		getService : function(name) {
 	    		if(Meteor.isServer) {
-	    			return services[name]; 
+	    			return services[name];
 	    		}
     		}
     	});
